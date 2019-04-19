3776c042af4f097e9b40a54ae3933eab2cce373b
Simon Genin <simongenin@hotmail.fr>
2019-04-19 22:20:15 +0200

    first migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('seller')->default('false');
+            $table->string('seller_name')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
